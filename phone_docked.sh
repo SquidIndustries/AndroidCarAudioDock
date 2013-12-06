b5fa04385b8cf220e1dcdace076f8bdfa0e968e6
Debian User <debian@debian-armhf.(none)>
2013-12-06 03:44:35 +0000

    changed the directory that the scripts point to. still hacky, will need to move these to permament home soon

--- phone_docked.sh
+++ phone_docked.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/debian/CarDock/android-usb-audio.py $1 $2
+/home/debian/AndroidCarAudioDock/android-usb-audio.py $1 $2
 #reduce volume so USB dac output isn't distorted
 pactl set-volume-sink alsa_output.usb-Burr-Brown_from_TI_USB_Audio_DAC-00-DAC.analog-stereo 70% &
 (sleep 3s ; pactl load-module module-loopback source=`pactl list sources short | grep alsa_input.usb | cut -f 1`) &
