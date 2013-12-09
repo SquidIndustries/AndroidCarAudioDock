ee9191a59e868c27657aa8b4e65d165ac9d5384f
Debian User <debian@debian-armhf.(none)>
2013-12-09 03:30:10 +0000

    -pulseaudio connection now made by /etc/rc.local at statup. Before no connection would be made if android device was connected at boot because udev populates before pulseaudio is started for necessary reasons. -HID command for play/pause sent to device now if connected at start up or if connected when already booted

--- phone_docked.sh
+++ phone_docked.sh
@@ -2,8 +2,18 @@
 
 /home/debian/AndroidCarAudioDock/android-usb-audio.py $1 $2
 #reduce volume so USB dac output isn't distorted
-pactl set-volume-sink alsa_output.usb-Burr-Brown_from_TI_USB_Audio_DAC-00-DAC.analog-stereo 70% &
-(sleep 3s ; pactl load-module module-loopback source=`pactl list sources short | grep alsa_input.usb | cut -f 1`) &
+
+#test to see if pulseaudio is running. if it is, set android device as input and then start play back via HID command
+if (ps -A | grep -q pulseaudio)
+then (pactl set-sink-volume 0 70%) & (sleep 3s ; pactl load-module module-loopback source=`pactl list sources short | grep alsa_input.usb | cut -f 1`) & (sleep 3s ; /home/debian/AndroidCarAudioDock/start_play.py 18d1 2d02)&
+fi
+
+
+#pactl set-volume-sink alsa_output.usb-Burr-Brown_from_TI_USB_Audio_DAC-00-DAC.analog-stereo 70% &
+#(sleep 3s ; pactl load-module module-loopback source=`pactl list sources short | grep alsa_input.usb | cut -f 1`) &
 #use following line instead of above for Fiio E10 USB Dac
 #(sleep 3s ; pactl load-module module-loopback source=3) &
+
+#start play back via HID command
+#(sleep 1s ; /home/debian/AndroidCarAudioDock/start_play.py 18d1 2d02)&
 
