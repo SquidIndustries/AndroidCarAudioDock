999b9b7bde3826a5cf619a6d8f30dc2c48ef6210
Debian User <debian@debian-armhf.(none)>
2014-02-02 03:23:25 +0000

    fixed call to python3 in script so that CAN code actually runs. Added some python logging stuff to see if the script was being called. Should expand code to include more logging

--- androiddocked.sh
+++ androiddocked.sh
@@ -2,7 +2,6 @@
 
 # Put android device into AOA2 usb audio out mode
 /usr/local/bin/aoa2usbaudio.py $1 $2
-/usr/local/bin/CANAOA2ctrl.py
 # wait 3 seconds for device to switch over, then connect android device to output device, wait 3seconds and send HID play/pause command
 # AOA2 is 16bit 2 channel, 44.1hkz
 # 50ms buffer
@@ -13,4 +12,4 @@
 
 
 (sleep 3s ; alsaloop -P hw:1,0,0 -C hw:2,0,0 -f S16_LE -r 44100 -t 50000 -S 3) & (sleep 3s ; /usr/local/bin/aoa2hid.py 18d1 2d02)&
-
+/usr/local/bin/python3 /usr/local/bin/CANAOA2ctrl.py &
