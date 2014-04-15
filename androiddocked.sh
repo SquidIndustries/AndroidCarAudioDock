6dacc8b5e2c0eaed4b838fae9cd983a1d3d3e61b
levesqu6 <blevesqu@gmail.com>
2014-04-14 23:59:42 -0400

    Update androiddocked.sh
    added while loop to wait for bus to come up

--- androiddocked.sh
+++ androiddocked.sh
@@ -2,6 +2,7 @@
 
 # Put android device into AOA2 usb audio out mode
 /usr/local/bin/aoa2usbaudio.py $1 $2
+
 # wait 3 seconds for device to switch over, then connect android device to output device, wait 3seconds and send HID play/pause command
 # AOA2 is 16bit 2 channel, 44.1hkz
 # 50ms buffer
@@ -9,18 +10,23 @@
 #	 5 or auto       - automatically selects the best method
 #	 in this order: captshift, playshift,
 #        samplerate, simple
-
-
 (sleep 3s ; alsaloop -P hw:1,0,0 -C hw:2,0,0 -f S16_LE -r 44100 -t 50000 -S 2)&
-#(sleep 3s ; /usr/local/bin/aoa2hid.py 18d1 2d02)
 
-#
-if /sbin/ifconfig | /bin/grep --quiet can0; then
-  #CAN is already setup, just run CAN interface code
-  /usr/local/bin/python3.3 /usr/local/bin/CANAOA2ctrl.py&
-else
-  #CAN isn't up, must be booting, wait for it to come up
-  (sleep 25s ; /usr/local/bin/python3.3 /usr/local/bin/CANAOA2ctrl.py)&
-fi
+#send play command to android device
+(sleep 3s ; /usr/local/bin/aoa2hid.py 18d1 2d02)
 
+#CAN stuff
+COUNTER=0
+while [  $COUNTER -lt 40 ]; do #try 40 times, 1 sec apart for CAN bus to come up
+  if /sbin/ifconfig | /bin/grep --quiet can0; then
+    #CAN bus is up, run CAN interface code
+    echo "CAN bus up, starting interface code"
+    /usr/local/bin/python3.3 /usr/local/bin/CANAOA2ctrl.py&
+  else
+    echo "CAN bus not up"
+  fi
+  sleep 1s #wait before trying again
+  let COUNTER=COUNTER+1
+done
+echo "gave up on waiting for CAN bus"
 exit 0
